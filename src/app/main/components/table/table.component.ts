6039097edb4fac9061152e70a8f82054590ddd8d
Cosmin George Iacob <George.Iacob@orange.com>
2021-03-22 04:44:36 +0200

    test generate cards

--- src/app/main/components/table/table.component.ts
+++ src/app/main/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -9,13 +9,43 @@ import {ActivatedRoute} from '@angular/router';
 export class TableComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) { }
+
   public id: number;
+  public cards: string[] ;
+  public cardsNumber: number;
+  public cursor: number;
+
+  private colors = ['C', 'D', 'H', 'S'];
+  private cardNumbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
   ngOnInit(): void {
+
     this.route.params.subscribe(params => {
-      const id = +params.id;
-      this.id = id;
+      this.id = +params.id;
     });
+    this.cardsNumber = 5;
+    this.cards = [];
+    for (let i = 0; i < this.cardsNumber; i++) {
+      this.cards.push('gray_back');
+    }
+    this.cursor = 0;
+  }
+
+  nextCard(): void {
+    if (this.cursor === 0) {
+      this.reset();
+    }
+    const randomColor = this.colors[Math.floor(Math.random() * (this.colors.length ))];
+    const randomCardNumber =  this.cardNumbers[Math.floor(Math.random() * (this.cardNumbers.length ))];
+    this.cards[this.cursor] =  randomCardNumber + randomColor;
+    this.cursor = (this.cursor + 1) % this.cardsNumber;
+  }
+
+  reset(): void {
+    for (let i = 0; i < this.cardsNumber; i++) {
+      this.cards[i] = 'gray_back';
+    }
+    this.cursor = 0;
   }
 
 }
