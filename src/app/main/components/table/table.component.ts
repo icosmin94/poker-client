c00c8c2c7ab6ffcc7a6bdee43de0b1aed9aa02fa
Cosmin George Iacob <George.Iacob@orange.com>
2021-04-02 22:47:26 +0300

    rsocket basic connection to rsocket server

--- src/app/main/components/table/table.component.ts
+++ src/app/main/components/table/table.component.ts
@@ -1,19 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Encodable, IdentitySerializer, JsonSerializer, RSocketClient} from 'rsocket-core';
+import RSocketWebSocketClient from 'rsocket-websocket-client';
+import {ReactiveSocket} from 'rsocket-types';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
-
+export class TableComponent implements OnInit, OnDestroy {
   constructor(private route: ActivatedRoute) { }
 
   public id: number;
   public cards: string[] ;
   public cardsNumber: number;
   public cursor: number;
+
+  public client: RSocketClient<Encodable, any>;
 
   private colors = ['C', 'D', 'H', 'S'];
   private cardNumbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
@@ -29,6 +33,48 @@ export class TableComponent implements OnInit {
       this.cards.push('green_back');
     }
     this.cursor = 0;
+
+    this.client = new RSocketClient({
+      serializers: {
+        data: JsonSerializer,
+        metadata: IdentitySerializer
+      },
+      setup: {
+        keepAlive: 60000,
+        lifetime: 180000,
+        dataMimeType: 'application/json',
+        metadataMimeType: 'message/x.rsocket.routing.v0',
+      },
+      transport: new RSocketWebSocketClient({
+        url: 'ws://localhost:7000/rsocket'
+      }),
+    });
+
+    this.client.connect().subscribe({
+      onComplete: (socket: ReactiveSocket<any, any>) => {
+        socket
+          .metadataPush( {
+            data: 'ana are mere',
+            metadata: String.fromCharCode('table-connection'.length) + 'table-connection'
+          })
+          .subscribe({
+            onComplete: () => console.log('complete'),
+            onError: error => {
+              console.log('Connection has been closed due to:: ' + error);
+            }
+          });
+
+      },
+      onError: error => {
+        console.log('Connection has been refused due to:: ' + error);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.client) {
+      this.client.close();
+    }
   }
 
   nextCard(): void {
